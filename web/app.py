381fb2695d104d73ea5361f21e76acf24148933e
mike <muhammedjepbarov@gmail.com>
2022-09-11 15:55:49 +0500

    refactoring app, added js html and create_app mode, need fix

--- web/app.py
+++ web/app.py
@@ -1,4 +1,7 @@
-from main import app
+from main_pack import create_app
+from main_pack.config import Config
+
+app = create_app()
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port = 5000)
--- web/main/__init__.py
+++ web/main/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+	from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_login import LoginManager
@@ -6,14 +6,21 @@ from main.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-CORS(app)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login_page'
 login_manager.login_message = 'Ulgama girin!'
 login_manager.login_message_category = 'info'
+
+
+
+def create_app(config_class=Config):
+	CORS(app)
+	db.init_app(app)
+	login_manager.init_app(app)
+
 
 from main.api import *
 from main.views import *
