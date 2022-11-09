49195607d43da8a3b1c6c8d5bf1ada8ffe65888d
mike <muhammedjepbarov@gmail.com>
2022-11-09 21:36:37 +0500

    updated templates

--- web/main/__init__.py
+++ web/main/__init__.py
@@ -12,16 +12,16 @@ login_manager.login_message = 'Ulgama girin!'
 login_manager.login_message_category = 'info'
 
 def create_app(config_class=Config):
-	app = Flask(__name__)
+	app = Flask(__name__,static_url_path=Config.STATIC_URL_PATH)
 	app.config.from_object(Config)
 	CORS(app)
 	db.init_app(app)
 	login_manager.init_app(app)
 
 	from main.api import api as main_api
-	app.register_blueprint(main_api, url_prefix='/api')
+	app.register_blueprint(main_api, url_prefix='/locator/api')
 
 	from main.views import bp as views_bp
-	app.register_blueprint(views_bp)
+	app.register_blueprint(views_bp, url_prefix='/locator')
 
 	return app
